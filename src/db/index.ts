f9f01c6167bbae028c2318f7621cd55f370cd17d
Tim <timjonsson03@hotmail.com>
2024-07-24 22:03:01 +0200

    V4 Push

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,6 @@ import Josh from "@joshdb/core";
 import provider from "@joshdb/mongo";
 import "dotenv/config";
 import type { Player } from "../lib/game/Player.js";
-import { Settings } from "../lib/bot/Settings.js";
 
 const uri = `mongodb+srv://Laptop:${process.env.MONGO_PASSWORD}@cluster0.bjnb7xh.mongodb.net/`;
 
