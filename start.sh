2a177619ea3df56c57e37759e3b004d38ff59a70
Tim <timjonsson03@hotmail.com>
2024-07-28 18:49:40 +0200

    Runs docs

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 # Run the build command
 npm run build
 
+# Build the docs & start server
+npx typedoc --options typedoc.json
+npx http-server --port 3334 docs/
+
 # Start the application with PM2
 node --es-module-specifier-resolution=node dist/index.js
