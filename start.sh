4ef67dafb9d9a69b65401fda63ffbffb00803ee4
Tim <timjonsson03@hotmail.com>
2024-07-28 18:54:23 +0200

    revert

--- start.sh
+++ start.sh
@@ -3,9 +3,5 @@
 # Run the build command
 npm run build
 
-# Build the docs & start server
-npx typedoc --options typedoc.json
-npx http-server --port 3334 docs/
-
 # Start the application with PM2
 node --es-module-specifier-resolution=node dist/index.js
