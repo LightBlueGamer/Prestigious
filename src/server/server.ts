8f2c37a8cdcb4be51efc38fe46f9eff35da7d62c
Tim <timjonsson03@hotmail.com>
2024-08-06 18:21:05 +0200

    possible README code stats fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,12 +3,15 @@ import "dotenv/config";
 import { router } from "./router/router.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { readdirSync } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.API_PORT || 3333;
+
+const assetFiles = readdirSync(join(__dirname, "../../public/assets"));
 
 app.use("/api", router);
 app.use(express.static(join(__dirname, "../../public")));
@@ -25,6 +28,12 @@ app.get(`/wiki/item`, (_req, res) => {
 app.get("/docs", (_req, res) => {
     res.sendFile(join(__dirname, "../../docs/index.html"));
 });
+
+for (const asset of assetFiles) {
+    app.get(`/assets/${asset}`, (_req, res) => {
+        res.sendFile(join(__dirname, "../../public/assets", asset));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
